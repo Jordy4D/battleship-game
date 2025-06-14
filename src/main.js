123afd11c0bc4568660f5fa2c2813206951695ad
Jordan Adams <jordan.adams@apexservicemw.com>
2025-06-14 16:10:09 -0400

    completes basic ship class, gameboard class init

--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
 export function testFn(num) {
     return num
 }
+
+export class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    hit() {
+        if (this.sunk !== true) {
+            return this.hits += 1
+        } 
+        
+        if (this.hits === this.length) {
+            return this.isSunk()
+        }
+    }
+
+    isSunk() {
+        return this.sunk = true;
+    }
+
+}
+
+export class Gameboard {
+    constructor() {
+        this.board = [];
+    }
+
+    createBoard(size) {
+        this.board = Array(size).fill(null).map(() => Array(size).fill(0));
+    }
+
+    receiveAttack(row, col) {
+        
+    }
+}
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,4 +1,4 @@
-import { testFn } from "../src/main.js"
+import { testFn, Ship } from "../src/main.js"
 
 test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
@@ -7,4 +7,22 @@ test('adds 1 + 2 to equal 3', () => {
 test('test function', () => {
     const t = testFn(1)
     expect(t).toBe(1);
+})
+
+test('checks Ship methods', () => {
+    const newShip = new Ship(3)
+
+    expect(newShip.length).toBe(3)
+    expect(newShip.hits).toBe(0)
+    expect(newShip.sunk).toBe(false)
+
+    newShip.hit()
+    expect(newShip.hits).toBe(1)
+    newShip.hit()
+    expect(newShip.hits).toBe(2)
+    newShip.hit()
+    expect(newShip.hits).toBe(3)
+    
+    newShip.isSunk()
+    expect(newShip.sunk).toBe(true)
 })
