86ce43f75f92d0cc5f6f12a429598da485301763
Jordy4D_LinuxVM <jsadams27@gmail.com>
2025-06-16 21:43:44 -0400

    expands placeship method, still working through how to handle

--- src/main.js
+++ src/main.js
@@ -29,16 +29,34 @@ export class Ship {
 export class Gameboard {
     constructor() {
         this.board = [];
+        this.ships = 0
     }
 
-    createBoard(size) {
+    createBoard(size, ships) {
         this.board = Array(size).fill(null).map(() => Array(size).fill(0));
+
+        this.ships = ships
     }
 
     //creates new ship at coordinates based on direction (vertical/horizontal)
     //coordinate provided should be the "front" of the ship
-    placeShip(row, col, dir) {
+    //Do we notate specific ships placed in the gameboard?
+    placeShip(length, row, col, dir) {
+        const ship = new Ship(length)
 
+        this.board[row][col] = 1
+        
+        //going to need to add edge limits for ship length, board edges, 
+        // and already placed ships
+        if (dir === v) {
+            for (let x = 0; x < length; x++) {
+                this.board[row + 1][col] = 1
+            }
+        } else if (dir === h) {
+            for (let x = 0; x < length; x++) {
+                this.board[row][col + 1] = 1
+            }
+        }
     }
 
     // 0 = empty space; 1 = ship; 2 = hit ship; 3 = sunken ship; 4 = missed attack
