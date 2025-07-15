51071184bcd2818b18c97248716c539c71b61b0a
Jordy4D_LinuxVM <jsadams27@gmail.com>
2025-07-14 22:36:13 -0400

    add gameplay.js file, basic config, bit more styling

---
+++ src/gameplay.js
@@ -0,0 +1,21 @@
+// import { testFn, Ship, Gameboard } from "../src/main.js"
+
+
+console.log("gameplay.js loaded");
+
+// export function startGame() {
+//     const playerName = prompt("Enter your name:");
+//     if (playerName) {
+//         document.getElementById("player-name").textContent = playerName;
+//     } else {
+//         document.getElementById("player-name").textContent = "Player";
+//     }
+
+//     const gameboard = new Gameboard();
+//     gameboard.createBoard(10, 5);
+    
+//     // Initialize the game UI and event listeners here
+//     // ...
+    
+//     console.log("Game started for", playerName);
+// }
